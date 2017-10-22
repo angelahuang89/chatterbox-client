dfa674fd1765112384b930205d657fa55a78b6ae
angelahuang89 <angelahuang89@gmail.com>
2017-10-21 17:51:25 -0700

    Continue improving Fatter Fox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,14 +3,15 @@ var App = function() {
   this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
   this.friends = [];
   this.results = 5;
+  this.currRoom = 'lobby';
 
   // this.init();
 };
 
-App.prototype.init = function() {
-  this.handleUsernameClick();
-  this.handleSubmit();
-};
+// App.prototype.init = function() {
+//   this.handleUsernameClick();
+//   this.handleSubmit();
+// };
 
 App.prototype.send = function(message) {
   $.ajax({
@@ -30,7 +31,7 @@ App.prototype.send = function(message) {
   });
 };
 
-App.prototype.fetch = function() {
+App.prototype.fetch = function(room = 'lobby') {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: this.server,
@@ -42,8 +43,13 @@ App.prototype.fetch = function() {
       console.log('w0000t');
       // debugger;
       // window.app.data = data;
+      $('select[id="roomSelect"]').change(function() {
+        this.currRoom = $(this).text();
+        App.prototype.goToRoom(this.currRoom, data.results);
+      });
       
       App.prototype.createRooms(data.results);
+      
       data.results.forEach(message => {
         App.prototype.renderMessage.call(this, message);
       });
@@ -53,7 +59,6 @@ App.prototype.fetch = function() {
       console.error('chatterbox: Failed to get message', data);
     }
   });
-  // console.log(results);
 };
 
 App.prototype.clearMessages = function() {
@@ -61,18 +66,30 @@ App.prototype.clearMessages = function() {
 };
 
 App.prototype.renderMessage = function(message) {
-  // if (!message.username.includes('<') && !message.text.includes('<')) {
-  // if (message.username && message.text) {
+    
+  // if (!message.username.includes('<') && !message.text.includes('>')) {
     $('#chats').append("<div value=" + "\'" + message.username + "\'" + " class='username'>" + message.username + "</div>");
     $('#chats').append("<div class='text'>" + message.text + "</div>");
+  // } 
   
   $('.username').click(function() {
     // console.log(this.text(message.username))
-    var friend = this.value;
-    console.log(this.value)
-    if (!app.friends.includes(this)) {
-      app.friends.push(this); 
-      $('#friends').append("<div class='friend'>" + this + "</div>");
+    var friend = $(this).text();
+    console.log(friend);
+    if (!app.friends.includes(friend)) {
+      app.friends.push(friend); 
+      $('#friends').append("<div class='friend'>" + friend + "</div>");
+    }
+  });
+};
+
+App.prototype.goToRoom = function(roomname, data) {
+  app.clearMessages();
+  app.fetch(roomname);
+  data.forEach(message => {
+    if (!message.roomname === roomname) {
+      console.log('hi');
+      message.hide();
     }
   });
 };
@@ -87,20 +104,19 @@ App.prototype.createRooms = function(data) {
       rooms.push(message.roomname);
     }
   });
-  console.log(rooms)
   // now have our uniq roomnames
   rooms.forEach(room => app.renderRoom(room));
 };
 
 App.prototype.renderRoom = function(room) {
-  $('#roomSelect').append($("<option>" + room + "</option>"));
+  $('#roomSelect').append($("<option value=" + "\'" + room + "\'" + ">" + room + "</option>"));
 };
 
 // were not using this method right now, not getting into it when we try. 
 // ask why 
 App.prototype.handleUsernameClick = function() {
   $('.username').click(function() {
-    console.log('we in hur')
+    console.log('we in hur');
     if (!app.friends.includes(this)) {
       app.friends.push(this);
     }
@@ -118,26 +134,18 @@ App.prototype.handleSubmit = function() {
   }.bind(this));
 };
 
-// App.prototype.refreshChats = function() {
-//   setInterval(
-//     app.fetch
-//   , 500);
-// };
-
 var app = new App();
 
 $(document).ready(function() {
-  // debugger;
   app.fetch();
   setInterval(function() { 
     app.clearMessages();
-    app.fetch(); }, 10000);
+    app.fetch(); 
+  }, 10000);
   app.handleSubmit();
   app.createRooms();
   app.handleUsernameClick();
   app.refreshChats();
-  // setInterval(app.fetch, 1000)();
-  // app.send({ username: 'i eat turtles', text: 'i am a turtle', roomname: 'general'})
 });
 
 
